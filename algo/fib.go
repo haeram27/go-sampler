52036faa855eb613b9c8838897b9115271b59016
haeram27 <haeram27@gmail.com>
2022-09-14 12:16:43 +0900

    add fibonacci test

--- algo/fib.go
+++ algo/fib.go
@@ -6,6 +6,9 @@ func Fibonacci(n uint) (uint64, error) {
 		return uint64(n), nil
 	}
 
+	// fibo(n) = fibo(n-1) + fibo(n-2)
+	// n1 : fibo(n-1)
+	// n2 : fibo(n-2)
 	var n2, n1 uint64 = 0, 1
 	for i := uint(2); i < n; i++ {
 		n2, n1 = n1, n1+n2
